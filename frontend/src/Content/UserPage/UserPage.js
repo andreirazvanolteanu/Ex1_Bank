55359d4265e0b21d4b79217fc2a061d43e390ea2
andreirazvanolteanu <andreirazvan.olteanu@solera.com>
2023-07-14 15:10:31 +0200

    Alternative commit

--- frontend/src/Content/UserPage/UserPage.js
+++ frontend/src/Content/UserPage/UserPage.js
@@ -4,11 +4,12 @@ import { useState, React } from "react";
 
 const baseURL = "/user/get-all";
 
+<script src='https://kit.fontawesome.com/a076d05399.js'></script>
 
 function UserPage() {
   
   const initialvalues = {
-    "id": "2",
+    "id": "9",
     "firstName": "",
     "lastName": "",
     "username": "",
@@ -46,38 +47,35 @@ function UserPage() {
 
     });
 
-
-
-
   return (
     <div className="UserPage">
+      <input type='checkbox' id='check' />
+      <label for='check'>
       
+      <i class="fas fa-times" id='cancel'>&times;</i>
+      </label>
+  
       <div className='User_SideBar'>
-            <p> {data.firstName} {data.lastName}</p>
-            <p> {data.username} </p>
+            <p id='firstname'> {data.firstName} {data.lastName}</p>
+            <p id='username'> {data.username} </p>
             <h3>$1,000.00</h3>
             <p>Account Balance</p>
-            <div className='links'>
-       <a href='/'>Home</a>
-       <br />
-       <br />
-       <a href='/user'>My Account</a>
-       <br />
-       <br />
-       <a href='/user'>Bank Accounts</a>
-       <br />
-       <br />
-       <a href='/'>Notifications</a>
-       <br />
-       <br />
-       <a href='/login'>Logout</a>
-
+            <ul>
+       <li><a href='/'>Home</a></li>
+       <li><a href='/user'>My Account</a></li>
+       <li><a href='/user'>Bank Accounts</a></li>
+       <li><a href='/'>Notifications</a></li>
+       <li><a href='/login'>Logout</a></li>
+      </ul>
       </div>
-      </div>
-      <div className='User_SetBar'>
+      
+      <div className='User_SetBar' id='User_SetBar'>
+      <label for='check'>
+      <i className='fas fa-bars' id='btn1'>☰</i>
+      </label>
             <em className='titleBar'>Real World App</em>
+            <button className='newBtn'>$ NEW</button>
       </div>
-     
 
     </div>
   );
